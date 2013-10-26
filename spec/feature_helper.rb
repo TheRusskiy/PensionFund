28c417e45779bcf75b41251fcfef1a7792dbaa13
TheRusskiy <dimanrussian@gmail.com>
2013-10-27 02:12:22 +0400

    capybara DSL now in config. Travis puts rspec docs in logs

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -2,7 +2,6 @@ require_relative 'spec_helper'
 require 'capybara/rails'
 require 'capybara/rspec'
 #Capybara.javascript_driver = :webkit
-include Capybara::DSL
 
 def sign_as_admin
   admin = create :user_admin
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.include Capybara::DSL
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
